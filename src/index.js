e68e112239f562a621a3fe77a3fbdc3a9a2ac622
Iryna Malash <mal7rish@gmail.com>
2023-05-16 21:45:44 +0500

    Use print statement middleware

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './api/server'
 import store from './store'
 
 store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-console.log('State after dispatch: ', store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
