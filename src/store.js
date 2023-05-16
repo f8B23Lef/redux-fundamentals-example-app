ac7d01118e3361227946a70668f3f090aa4e5ca2
Iryna Malash <mal7rish@gmail.com>
2023-05-16 22:04:13 +0500

    Use composeWithDevTools to enable the DevTools Extension

--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 import {
   loggerMiddleware,
   delayedMessageMiddleware,
 } from './exampleAddons/middleware'
 
-const middlewareEnhancer = applyMiddleware(
-  loggerMiddleware,
-  delayedMessageMiddleware
+const composedEnhancer = composeWithDevTools(
+  // EXAMPLE: Add whatever middleware you actually want to use here
+  applyMiddleware(loggerMiddleware, delayedMessageMiddleware)
+  // other store enhancers if any
 )
 
 // Pass enhancer as the second arg, since there's no preloadedState
-const store = createStore(rootReducer, middlewareEnhancer)
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
