4f77aad7dc53adbe16dac21b6acd2a7f46c7d7dc
Iryna Malash <mal7rish@gmail.com>
2023-05-16 21:37:11 +0500

    Add and use meaningOfLife store enhancer

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import './api/server'
 
 import store from './store'
 
-console.log('Dispatching action')
 store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-console.log('Dispatch complete')
+console.log('State after dispatch: ', store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
