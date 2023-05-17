edadc1de90f159852faa1d44dfd1634da76fc949
Iryna Malash <mal7rish@gmail.com>
2023-05-17 20:53:17 +0500

    Convert TodoList to pass item IDs to TodoListItem

--- src/features/todos/TodoListItem.js
+++ src/features/todos/TodoListItem.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { ReactComponent as TimesSolid } from './times-solid.svg'
 
 import { availableColors, capitalize } from '../filters/colors'
 
-const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
+const selectTodoById = (state, todoId) => {
+  return state.todos.find((todo) => todo.id === todoId)
+}
+
+const TodoListItem = ({ id }) => {
+  console.log('--TodoListItem()--', id)
+  const todo = useSelector((state) => selectTodoById(state, id))
   const { text, completed, color } = todo
 
-  const handleCompletedChanged = (e) => {
-    onCompletedChange(e.target.checked)
+  const dispatch = useDispatch()
+
+  const handleCompletedChanged = () => {
+    dispatch({ type: 'todos/todoToggled', payload: todo.id })
   }
 
-  const handleColorChanged = (e) => {
-    onColorChange(e.target.value)
-  }
+  const handleColorChanged = () => {}
+  const onDelete = () => {}
 
   const colorOptions = availableColors.map((c) => (
     <option key={c} value={c}>
