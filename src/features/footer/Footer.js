db15ada2c8cf21a5d919d397ab877d5012db7d73
Iryna Malash <mal7rish@gmail.com>
2023-05-21 16:59:29 +0500

    Add action creator for colorFilterChanged

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { availableColors, capitalize } from '../filters/colors'
-import { StatusFilters } from '../filters/filtersSlice'
+import { StatusFilters, colorFilterChanged } from '../filters/filtersSlice'
 
 const RemainingTodos = ({ count }) => {
   const suffix = count === 1 ? '' : 's'
@@ -91,13 +91,7 @@ const Footer = () => {
   }
 
   const onColorChange = (color, changeType) => {
-    dispatch({
-      type: 'filters/colorFilterChanged',
-      payload: {
-        color,
-        changeType,
-      },
-    })
+    dispatch(colorFilterChanged(color, changeType))
   }
 
   const onStatusChange = (status) => {
