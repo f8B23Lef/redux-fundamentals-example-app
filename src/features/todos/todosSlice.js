e7c031ccda0715bca7a36fd04e8a95366a879058
Iryna Malash <mal7rish@gmail.com>
2023-05-21 17:05:49 +0500

    Shorten fetchTodos and todoAdded using arrow functions

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -63,19 +63,12 @@ export const todosLoaded = (todos) => {
   }
 }
 
-export const todoAdded = (todo) => {
-  return {
-    type: 'todos/todoAdded',
-    payload: todo,
-  }
-}
+export const todoAdded = (todo) => ({ type: 'todos/todoAdded', payload: todo })
 
 // Thunk function
-export function fetchTodos() {
-  return async function fetchTodosThunk(dispatch, getState) {
-    const response = await client.get('/fakeApi/todos')
-    dispatch(todosLoaded(response.todos))
-  }
+export const fetchTodos = () => async (dispatch) => {
+  const response = await client.get('/fakeApi/todos')
+  dispatch(todosLoaded(response.todos))
 }
 
 // Write a synchronous outer function that receives the `text` parameter:
