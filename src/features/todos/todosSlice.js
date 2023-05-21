f88430e70c2c00d267f006988417a245a4841e6d
Iryna Malash <mal7rish@gmail.com>
2023-05-21 16:45:14 +0500

    Add and use saveNewTodo thunk

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -6,22 +6,11 @@ const initialState = [
   // { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
 ]
 
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
-
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      return [
-        ...state,
-        {
-          id: nextTodoId(state),
-          text: action.payload,
-          completed: false,
-        },
-      ]
+      // Return a new todos state array with the new todo item at the end
+      return [...state, action.payload]
     }
     case 'todos/todoToggled': {
       return state.map((todo) => {
@@ -78,4 +67,15 @@ export async function fetchTodos(dispatch, getState) {
 
   const stateAfter = getState()
   console.log('Todos after dispatch: ', stateAfter.todos.length)
+}
+
+// Write a synchronous outer function that receives the `text` parameter:
+export function saveNewTodo(text) {
+  // And then creates and returns the async thunk function:
+  return async function saveNewTodoThunk(dispatch, getState) {
+    // ✅ Now we can use the text value and send it to the server
+    const initialTodo = { text }
+    const response = await client.post('/fakeApi/todos', { todo: initialTodo })
+    dispatch({ type: 'todos/todoAdded', payload: response.todo })
+  }
 }
